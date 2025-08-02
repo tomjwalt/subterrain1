a3b3b4b1e50afa0612001e92ccd047a77859e356
tomjwalt <tomjwalton123@gmail.com>
2025-08-02 16:55:36 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <div className="title">
+        <h1>Subterrain</h1>
+        <a href="./Navbar.jsx"></a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
