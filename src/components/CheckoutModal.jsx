250b82747e2015297d3055cd3498e72c1f14a2fb
tomjwalt <tomjwalton123@gmail.com>
2025-08-27 19:40:29 +0100

    initial commit

--- src/components/CheckoutModal.jsx
+++ src/components/CheckoutModal.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Checkout from "./Checkout";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faUser, faXmark  } from "@fortawesome/free-solid-svg-icons";
 
 const stripePromise = loadStripe("pk_test_YOUR_KEY_HERE");
 
@@ -14,7 +16,7 @@ const CheckoutModal = ({ onClose }) => {
           onClick={onClose}
           className="absolute top-4 right-4 text-xl font-bold text-gray-500 hover:text-gray-200"
         >
-          ×
+          <FontAwesomeIcon icon={faXmark} />
         </button>
 
         {/* Title */}
