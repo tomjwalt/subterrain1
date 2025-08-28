7473964df6aa358b9b0768715c5beaa91e0756f2
tomjwalt <tomjwalton123@gmail.com>
2025-08-28 22:26:48 +0100

    added supabase and other shet

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://nyebwdvhkgiumqswbrfb.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
