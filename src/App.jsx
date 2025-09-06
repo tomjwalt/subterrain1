b07ef75f555e102d33d1cf1edc885cd6e29f964a
tomjwalt <tomjwalton123@gmail.com>
2025-09-06 08:06:06 +0100

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import Checkout from "./components/Checkout.jsx";
 import Login from "./components/Login.jsx";
+import signUp from "./components/Signup.jsx"
 
 const stripePromise = loadStripe(
   "***REMOVED***_51RsQkwQzbCrsaJqpklbrEN3mFg28PeiakQ5byiLctR6TCusfX2o9wcRRSqpX2RPbAwn5sV2WhRMkINAmYZPZ5TWv00ErkukHDt"
