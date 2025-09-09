841d4faf90d870aff66e92c9e15a1b81965fb85f
tomjwalt <tomjwalton123@gmail.com>
2025-09-09 21:00:05 +0100

    bug fixes, added fields to sign up, css changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { supabase } from "../../supabaseClient";
-import { Link, Routes, Route } from "react-router-dom"
+import { Link, Routes, Route } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [gender, setGender] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [successMsg, setSuccessMsg] = useState("");
 
@@ -14,7 +21,10 @@ const Signup = () => {
     setErrorMsg("");
     setSuccessMsg("");
 
-    const { data: { user }, error } = await supabase.auth.signUp({
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.signUp({
       email,
       password,
     });
@@ -34,7 +44,9 @@ const Signup = () => {
         setErrorMsg("Signed up but failed to create profile.");
         console.error(profileError.message);
       } else {
-        setSuccessMsg("Sucessfully signed up check email to authenticate account.");
+        setSuccessMsg(
+          "Sucessfully signed up check email to authenticate account."
+        );
       }
     }
   };
@@ -42,29 +54,66 @@ const Signup = () => {
   return (
     <form onSubmit={handleSignUp} className="flex flex-col gap-4">
       <input
+        type="first name"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setfirstName(e.target.value)}
+        className="text-white border border-gray-400 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+      />
+      <input
+        type="last name"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        className="text-white border border-gray-400 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+      />
+      <input
         type="email"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="text-white border p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="text-white border border-gray-400 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
       />
+      <div className="relative">
+        <input
+          id="dob"
+          type="date"
+          className="dob-input border border-gray-400 p-2 rounded text-white w-full pr-10"
+        />
+        <FontAwesomeIcon
+          icon={faCalendarDays}
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-white pointer-events-none"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="gender" className="text-gray-300 text-sm">
+          Gender
+        </label>
+        <select
+          value={gender}
+          id="gender"
+          onChange={(e) => setGender(e.target.value)}
+          className="text-white border border-gray-400 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+        >
+          <option value="" disabled>
+            Select Gender
+          </option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="faggot">Faggot</option>
+        </select>
+      </div>
       <input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className="text-white border p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="text-white border border-gray-400 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
       />
-      <input
-        type="name"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className="text-white border p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-      />
+
       <button
         type="submit"
-        className="bg-green-600 text-white py-2 rounded hover:bg-green-700 transition cursor-pointer"
+        className="border border-gray-400 bg-green-600 text-gray-300 py-2 rounded hover:text-white transition cursor-pointer"
       >
         Sign Up
       </button>
